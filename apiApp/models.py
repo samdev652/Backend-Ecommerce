49610b9305558ee0aef51b1054d53e6f1b22903e
sambaraks652 <sambaraks652@gmail.com>
2025-10-30 23:22:24 +0300

    done with my models.py

--- apiApp/models.py
+++ apiApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
@@ -13,11 +14,24 @@ class customUser(AbstractUser):
     
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True, blank=True)
     image = models.ImageField(upload_to='category_img', blank=True, null=True) 
 
     def __str__(self):
         return self.name  
+    
+def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            unique_slug = self.slug
+            counter = 1
+            if Product.objects.filter(slug=unique_slug).exists():
+                unique_slug = f"{self.slug}-{counter}"
+                counter += 1
+            self.slug = unique_slug
+
+        super().save(*args, **kwargs
+        )    
     
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -30,3 +44,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            unique_slug = self.slug
+            counter = 1
+            if Product.objects.filter(slug=unique_slug).exists():
+                unique_slug = f"{self.slug}-{counter}"
+                counter += 1
+            self.slug = unique_slug
+
+        super().save(*args, **kwargs
+        )
